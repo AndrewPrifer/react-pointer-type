fb3b067cb98bc9c634cb5993ac3c18b21bdf2c7b
Andrew Prifer <andrew.prifer@gmail.com>
2021-09-07 13:56:19 +0200

    Overhaul API

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,29 +1,17 @@
 import React from 'react';
 import styles from './App.module.css';
-import {
-  useMatchPointerType,
-  useMatchPointerTypeClassName,
-  usePointerType,
-  usePointerTypeClassName,
-} from '../../';
+import { usePointerType } from '../../';
 
 function App() {
-  const inputType = usePointerType();
-  const match = useMatchPointerType(['touch', 'pen']);
-  usePointerTypeClassName();
-  useMatchPointerTypeClassName(['touch', 'pen'], {
-    match: 'nohover',
+  const inputType = usePointerType({
+    attachClass: true,
   });
 
   return (
     <div>
       <div>Current pointer type: {inputType}</div>
-      <div>Matching pen or touch: {String(match)}</div>
       <div className={styles.pointerIndicator}>
         This div is red for mouse, blue for touch and green for pen.
-      </div>
-      <div className={styles.matchIndicator}>
-        This div is green if pen or touch are matched, otherwise red.
       </div>
     </div>
   );
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,30 @@ import { useEffect, useState } from 'react';
 
 export type PointerType = 'mouse' | 'pen' | 'touch';
 
+type PointerEventType =
+  | 'pointerover'
+  | 'pointerenter'
+  | 'pointerdown'
+  | 'pointermove'
+  | 'pointerup'
+  | 'pointercancel'
+  | 'pointerout'
+  | 'pointerleave'
+  | 'gotpointercapture'
+  | 'lostpointercapture';
+
 interface UsePointerTypeOptions {
   initial?: PointerType;
+  attachClass?: boolean;
+  classPrefix?: string;
+  updateOn?: PointerEventType;
 }
 
 export const usePointerType = ({
   initial = 'mouse',
+  attachClass = false,
+  classPrefix = '',
+  updateOn = 'pointerdown',
 }: UsePointerTypeOptions = {}) => {
   const [pointerType, setPointerType] = useState<PointerType>(initial);
 
@@ -16,83 +34,26 @@ export const usePointerType = ({
       setPointerType(e.pointerType as PointerType);
     };
 
-    document.addEventListener('pointerdown', handler);
+    document.addEventListener(updateOn, handler);
 
     return () => {
-      document.removeEventListener('pointerdown', handler);
+      document.removeEventListener(updateOn, handler);
     };
   }, []);
 
+  useEffect(() => {
+    if (attachClass) {
+      document.documentElement.classList.add(classPrefix + pointerType);
+
+      return () => {
+        document.documentElement.classList.remove(classPrefix + 'mouse');
+        document.documentElement.classList.remove(classPrefix + 'touch');
+        document.documentElement.classList.remove(classPrefix + 'pen');
+      };
+    }
+
+    return;
+  }, [pointerType, classPrefix, attachClass]);
+
   return pointerType;
-};
-
-interface UseMatchPointerTypeOptions {
-  initial?: PointerType;
-}
-
-export const useMatchPointerType = (
-  pointerTypes: PointerType[],
-  { initial = 'mouse' }: UseMatchPointerTypeOptions = {}
-) => {
-  const type = usePointerType({ initial });
-
-  return pointerTypes.includes(type);
-};
-
-interface UsePointerTypeClassNameOptions {
-  prefix?: string;
-  initial?: PointerType;
-}
-
-export const usePointerTypeClassName = ({
-  prefix = '',
-  initial = 'mouse',
-}: UsePointerTypeClassNameOptions = {}) => {
-  const type = usePointerType({ initial });
-
-  useEffect(() => {
-    document.documentElement.classList.add(prefix + type);
-
-    return () => {
-      document.documentElement.classList.remove(prefix + 'mouse');
-      document.documentElement.classList.remove(prefix + 'touch');
-      document.documentElement.classList.remove(prefix + 'pen');
-    };
-  }, [type, prefix]);
-};
-
-interface UseMatchPointerTypeClassNameOptions {
-  match?: string;
-  noMatch?: string;
-  initial?: PointerType;
-}
-
-export const useMatchPointerTypeClassName = (
-  pointerTypes: PointerType[],
-  {
-    match: matchClass,
-    noMatch: noMatchClass,
-    initial = 'mouse',
-  }: UseMatchPointerTypeClassNameOptions
-) => {
-  const match = useMatchPointerType(pointerTypes, { initial });
-
-  useEffect(() => {
-    if (match && matchClass) {
-      document.documentElement.classList.add(matchClass);
-    }
-
-    if (!match && noMatchClass) {
-      document.documentElement.classList.add(noMatchClass);
-    }
-
-    return () => {
-      if (matchClass) {
-        document.documentElement.classList.remove(matchClass);
-      }
-      if (noMatchClass) {
-        document.documentElement.classList.remove(noMatchClass);
-      }
-    };
-  }, [match, matchClass, noMatchClass]);
 };
